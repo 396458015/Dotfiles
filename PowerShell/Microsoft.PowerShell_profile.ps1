21152fde5e9040fd1463d77def5a1351c321e981
396458015 <396458015@qq.com>
2025-02-09 23:19:07 +0800

    add powershell

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,317 @@
+# PowerShell settings
+
+# Import the module
+
+# ------------------- 颜色设置 -------------------
+# 下载"https://github.com/catppuccin/powershell"主题到C:\Users\ThinkPad\Documents\PowerShell\Modules\Catppuccin
+Import-Module Catppuccin
+# $Flavor = $Catppuccin['Frappe']
+$Flavor = $Catppuccin['Latte']
+# Set the colours of PSReadLine
+$Colors = @{
+	# Largely based on the Code Editor style guide
+	# Emphasis, ListPrediction and ListPredictionSelected are inspired by the Catppuccin fzf theme
+	
+	# Powershell colours
+	ContinuationPrompt     = $Flavor.Teal.Foreground()
+	Emphasis               = $Flavor.Red.Foreground()
+	Selection              = $Flavor.Surface0.Background()
+	
+	# PSReadLine prediction colours
+	InlinePrediction       = $Flavor.Overlay0.Foreground()
+	ListPrediction         = $Flavor.Mauve.Foreground()
+	ListPredictionSelected = $Flavor.Surface0.Background()
+
+	# Syntax highlighting
+	Command                = $Flavor.Blue.Foreground()
+	Comment                = $Flavor.Overlay0.Foreground()
+	Default                = $Flavor.Text.Foreground()
+	Error                  = $Flavor.Red.Foreground()
+	Keyword                = $Flavor.Mauve.Foreground()
+	Member                 = $Flavor.Rosewater.Foreground()
+	Number                 = $Flavor.Peach.Foreground()
+	Operator               = $Flavor.Sky.Foreground()
+	Parameter              = $Flavor.Pink.Foreground()
+	String                 = $Flavor.Green.Foreground()
+	Type                   = $Flavor.Yellow.Foreground()
+	Variable               = $Flavor.Lavender.Foreground()
+}
+Set-PSReadLineOption -Colors $Colors
+
+# ------------------- 设置yazi环境(0.2.5) -------------------
+$Env:YAZI_FILE_ONE = "D:\Program Files\Git\usr\bin\file.exe"
+
+# 解决fzf查询结果含有CJK,路径乱码nvim打不开的情况
+# set PowerShell to UTF-8
+$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+Invoke-Expression (&starship init powershell)
+Import-Module Terminal-Icons
+
+#PSReadline settings
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+# Set-PSReadLineOption -PredictionViewStyle InlineView
+# Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+# Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineKeyHandler -Key Shift+Tab -Function TabCompletePrevious
+# Set-PSReadlineKeyHandler -Key "Ctrl+w" -Function ViExit
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
+Set-PSReadLineKeyHandler -Key Ctrl+Backspace -Function BackwardKillWord
+Set-PSReadLineKeyHandler -Chord "Ctrl+j" -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord "Ctrl+k" -Function HistorySearchBackward
+
+# Alias
+Set-Alias v nvim
+Set-Alias gg LazyGit
+Set-Alias ipy ipython
+Set-Alias rename Rename-Item
+
+# 打开子系统,路径为~
+function ww {
+    wsl --cd /home/thinkpad
+}
+
+# 复制当前路径 /
+function Copy-WorkingDirectory {
+    $path = (Get-Location).Path
+    $quotedPath = "`'$path`'"
+    $quotedPath | Set-Clipboard
+    Write-Host "`nCopied path to clipboard:`n`n$quotedPath`n" #`n是换行符
+    # Write-Host "`nCopied path to clipboard:`n`n$Path`n" #`n是换行符
+}
+Set-Alias -Name / -Value Copy-WorkingDirectory
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+# 查看目录 ls & ll
+function ListDirectory {
+	(Get-ChildItem).Name
+	Write-Host("")
+}
+Set-Alias -Name ls -Value ListDirectory
+Set-Alias -Name ll -Value Get-ChildItem
+
+# 树状图
+function list_tree{ tree /F }
+Set-Alias -Name lt -Value list_tree
+
+# Computer info
+Set-Alias fe winfetch
+
+# Basic commands
+function touch($file) { "" | Out-File $file -Encoding ASCII }
+function q { exit }
+function cdd {cd C:\Users\ThinkPad\Desktop}
+function md { mkdir }
+function rm { Reomve-Item }
+function .. { cd .. }
+function ... { cd ..; cd ..}
+function .... { cd ..; cd ..; cd .. }
+function ..... { cd ..; cd ..; cd ..; cd ..}
+
+# 当前工作目录以我的电脑形式打开, o
+function OpenCurrentFolder {
+	param
+	(
+		$Path = '.'
+	)
+	Invoke-Item $Path
+}
+Set-Alias -Name o -Value OpenCurrentFolder
+
+function dt { cd '~/Desktop' }
+
+# 修改 PSreadLine 历史记录, 删除错误的记录
+# (Get-PSReadLineOption).HistorySavePath  可以获得历史记录文件路径
+function edith { nvim -c 'normal! G' (Get-PSReadLineOption).HistorySavePath }
+Set-PSReadLineKeyHandler -Chord ctrl+e -ScriptBlock {
+    [Microsoft.Powershell.PSConsoleReadline]::RevertLine()
+    [Microsoft.Powershell.PSConsoleReadline]::Insert("edith")
+    [Microsoft.Powershell.PSConsoleReadline]::AcceptLine()
+}
+# ------------------- open app -------------------
+
+function alacritty_start {
+    $alacritty_path = "C:\Users\ThinkPad\scoop\apps\alacritty\current\alacritty.exe"
+    Start-Process $alacritty_path
+}
+Set-Alias -Name alacritty -Value alacritty_start
+
+function start_wezterm {
+    $wezterm_path = "D:\Program Files\WezTerm\wezterm-gui.exe"
+    Start-Process $wezterm_path
+}
+Set-Alias -Name wezterm -Value start_wezterm
+
+function sumatrapdf_start {
+    $sumatrapdf_path = "C:\Users\ThinkPad\scoop\apps\SumatraPDF\current\SumatraPDF.exe"
+    Start-Process $sumatrapdf_path
+}
+Set-Alias -Name sumatra -Value sumatrapdf_start
+
+function word_start {
+    $word_path = "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE"
+    Start-Process $word_path
+}
+Set-Alias -Name word -Value word_start
+
+function ppt_start {
+    $ppt_path = "C:\Program Files\Microsoft Office\root\Office16\POWERPNT.EXE"
+    Start-Process $ppt_path
+}
+Set-Alias -Name ppt -Value ppt_start
+
+function excel_start {
+    $excel_path = "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE"
+    Start-Process $excel_path
+}
+Set-Alias -Name excel -Value excel_start
+
+function chrome_start {
+    $chrome_path = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+    Start-Process $chrome_path
+}
+Set-Alias -Name chrome -Value chrome_start
+
+function niz_start {
+    $niz_path = "F:\package_tools\NIZ\23年6月编程固件说明校验软件包\23年6月编程固件说明校验软件包\micro82编程软件 V1.5.1\82EC(XRGB)Ble.exe"
+    Start-Process $niz_path
+}
+Set-Alias -Name niz -Value niz_start
+
+function geek_start {
+    $geek_path = "D:\Program Files\geek\geek.exe"
+    Start-Process $geek_path
+}
+Set-Alias -Name geek -Value geek_start
+
+
+# ------------------- config files -------------------
+# wezterm
+function weconfig { nvim 'C:\Users\ThinkPad\.config\wezterm\config\appearance.lua' }
+# alacritty
+function alconfig { nvim 'C:\Users\ThinkPad\AppData\Roaming\alacritty\alacritty.toml' }
+# powershell
+function psconfig { nvim $PROFILE }
+# starship
+function ssconfig { nvim 'C:\Users\ThinkPad\Documents\PowerShell\starship.toml' }
+# nvim
+function viconfig { nvim 'C:\Users\ThinkPad\AppData\Local\nvim\init.lua' }
+# windows terminal
+function wtconfig { nvim 'C:\Users\ThinkPad\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json' }
+# goneovim
+function goconfig { nvim 'C:\Users\ThinkPad\AppData\Roaming\.goneovim\settings.toml' }
+# winfetch
+function wfconfig { nvim 'C:\Users\ThinkPad\.config\winfetch\config.ps1' }
+# yazi
+function yzconfig { nvim 'C:\Users\ThinkPad\AppData\Roaming\yazi\config\keymap.toml' }
+# lazygit
+function lgconfig { nvim 'C:\Users\ThinkPad\AppData\Roaming\lazygit\config.yml' }
+
+# ------------------- fzf + nvim + bat -------------------
+#█▓▒░ fzf
+$fzf_opts = @(
+    # "--multi",
+    # "--reverse",
+    # "--exact",
+    "--height 100%",
+    "--preview-window=right:55%",
+    "--header='      '",
+    "--prompt=''",#
+    "--marker='󰸞'",#✓
+    "--pointer='▶'",#➤▶
+    "--info=inline:'󰶺  '",
+    "--no-separator --scrollbar='▐'",
+    "--ansi",
+    "--cycle",
+    "--border=rounded --border-label='󰞘  󰞗'",
+    "--margin=0,0",
+    "--preview 'bat --theme=TwoDark --color=always --style=numbers --line-range :500 {}'",
+    # "--color=fg:#abb2bf,bg+:#343d46,gutter:-1,pointer:#ff5189,info:#f09479,hl:#36c692,hl+:#36c692,label:#80a0ff", # Frappe
+    # "--color=marker:#f09479,spinner:#36c692,header:#80a0ff,fg+:#cdd6f4,prompt:#87afff,border:#51576d", # Frappe
+    "--color=fg:$($Flavor.Text),bg+:$($Flavor.Surface0),gutter:-1,pointer:$($Flavor.Rosewater),info:$($Flavor.Mauve),hl:$($Flavor.Red),hl+:$($Flavor.Red),label:#80a0ff", # catppuccin
+    "--color=marker:$($Flavor.Rosewater),spinner:$($Flavor.Rosewater),header:$($Flavor.Red),fg+:$($Flavor.Text),prompt:$($Flavor.Mauve),border:$($Flavor.Surface2)", # catppuccin
+    "--bind ctrl-p:toggle-preview",
+    "--bind ctrl-j:down",
+    "--bind ctrl-k:up",
+    "--bind ctrl-s:toggle-sort",
+    "--bind ctrl-f:preview-half-page-down",
+    "--bind ctrl-b:preview-half-page-up",
+    "--bind ctrl-a:select-all"
+    # "--bind ctrl-u:preview-up",
+    # "--bind ctrl-d:preview-down",
+    # "--bind ctrl-f:page-down",
+    # "--bind ctrl-b:page-up"
+)
+Set-Item Env:FZF_DEFAULT_OPTS -Value ($fzf_opts -join " ")
+
+# 打开fzf用nvim打开文件
+function Invoke-FZF() {
+    $result = . (Get-Command -CommandType Application fzf) $args
+    if ($result) {
+        nvim $result
+    }
+}
+Set-PSReadLineKeyHandler -Chord ctrl+f -ScriptBlock {
+    [Microsoft.Powershell.PSConsoleReadline]::RevertLine()
+    [Microsoft.Powershell.PSConsoleReadline]::Insert("Invoke-FZF")
+    [Microsoft.Powershell.PSConsoleReadline]::AcceptLine()
+}
+
+# 打开fzf并且cd进去
+Set-PSReadlineKeyHandler -Chord ctrl+g -ScriptBlock {
+  [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+  [Microsoft.PowerShell.PSConsoleReadLine]::Insert('cd "$(fzf)\.."')
+  [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
+function cf {cd "$(fzf)\.."}
+
+# ------------------- yazi -------------------
+# Changing working directory when exiting Yazi
+function yzcd {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -Path $cwd
+    }
+    Remove-Item -Path $tmp
+}
+
+Set-PSReadLineKeyHandler -Chord Alt+f -ScriptBlock {
+    [Microsoft.Powershell.PSConsoleReadline]::RevertLine()
+    [Microsoft.Powershell.PSConsoleReadline]::Insert("yzcd")
+    [Microsoft.Powershell.PSConsoleReadline]::AcceptLine()
+}
+
+# ------------------- wezterm -------------------
+# wezterm图片预览
+function img { wezterm imgcat $args }
+
+# ------------------- alacritty -------------------
+# alacritty映射特性，有缺陷,vim的insert状态下输入快捷键会输出'F10,a'等
+# 因此，最终映射ALT-h j k l 为左下上右
+# Set-PSReadLineKeyHandler -Chord 'F10,a' -Function HistorySearchForward
+# Set-PSReadLineKeyHandler -Chord 'F10,b' -Function HistorySearchBackward
+
+
+# ni 新建文本
+# ren 重命名
+
+
+
+
+
+
+
---
+++ PowerShell/readme.md
@@ -0,0 +1,203 @@
+# 安装powershell
+    双击ps安装程序.
+
+# 配置powershell
+  1. powershell配置文件'Microsoft.PowerShell_profile.ps1'  
+     安装路径：'C:\Program Files\PowerShell\7'  
+     配置文件路径：'C:\Users\ThinkPad\Documents\PowerShell\Microsoft.PowerShell_profile.ps1'  
+     快捷命令'psconfig'  
+  2. 设置powershell主题  
+     下载"https://github.com/catppuccin/powershell"主题到"C:\Users\ThinkPad\Documents\PowerShell\Modules\Catppuccin"
+  3. 安装scoop, 通过scoop安装插件  
+     3.1 Open powershell  
+     3.2 Input 'Set-ExecutionPolicy RemoteSigned -scope CurrentUser' 输入y  
+     3.3 Input 'iwr -useb https://gitee.com/glsnames/scoop-installer/raw/master/bin/install.ps1 | iex'  
+  4. 安装 Icons  
+     通过scoop安装 (推荐),Open powershell  
+         'scoop bucket add extras'  
+         'scoop install terminal-icons'  
+     或通过PSGallery安装  
+         Open powershell  
+         Input 'Install-Module -Name Terminal-Icons -Repository PSGallery -Force'  
+  5. 通过scoop安装tree-sitter,neovim中treesitter的latex需要  
+         'scoop install tree-sitter'  
+  6. 通过scoop安装winfetch  
+         'scoop bucket add extras'  
+         'scoop install winfetch'  
+     * alias wf (winfetch)  
+     * 配置文件路径:'C:\Users\ThinkPad\.config\winfetch\config.ps1'  
+     * 配置文件快捷键:'wfconfig'  
+     * winfetch仓库:https://github.com/lptstr/winfetch  
+     * winfetch安装介绍:https://github.com/lptstr/winfetch/wiki/Installation  
+  7. 安装 starship  
+     7.1 Open powershell  
+     7.2 Input  'scoop install starship'(推荐) 或者 'winget install starship'  
+     7.3 系统变量,设置变量名:'STARSHIP_CONFIG', 变量值:'C:\Users\ThinkPad\Documents\PowerShell\starship.toml'  
+     7.4 配置文件'starship.toml'.  
+         路径：'C:\Users\ThinkPad\Documents\PowerShell\starship.toml'  
+     7.5 快捷命令'ssconfig'  
+  8. 安装fzf  
+     8.1 Open powershell  
+     8.2 Input 'scoop install fzf'  
+     快捷键 ctrl+f ctrl+g  
+     注意：fzf不能正确获得名称或路径含中文字符，将powershell设置为utf8格式可以解决该问题.  
+  9. 安装bat  
+     9.1 Open powershell  
+     9.2 Input 'scoop install bat'  
+     9.3 'bat cache --build'  说明网址'https://github.com/sharkdp/bat#adding-new-syntaxes--language-definitions'
+     use 'bat --list-languages' to check if the new languages are available
+     if you ever want to go back to the default setting, 'bat cache --clear'
+     配置文件路径:'C:\Users\ThinkPad\AppData\Local\bat'
+     分别在powershell配置文件添加代码预览bat的配置  
+     采用bat实现fzf的代码预览含高亮.ps:yazi自身集成了代码预览  
+     查看bat配色主题,'bat --list-themes'
+  10. install Directory jumper z (通过scoop)  
+     10.1 Open powershell  
+     10.2 Input 'scoop install z'  
+  11. 由于版本自带命令提示插件'PSReadLine',无需安装.  
+  12. 安装yazi.  
+     12.1. Download Git from https://git-scm.com/download/win.  
+     12.2. Append C:\Program Files\Git\usr\bin to the PATH environment variable, note that depends on your Git installation path.  
+     例如将D:\Program Files\Git\usr\bin加入到环境变量，同时加入到'用户变量'和'系统变量'的path  
+     12.3. scoop install yazi  
+     12.4 for zip/rar preview  
+        scoop install unar  
+     12.5 for JSON preview  
+        scoop install jq  
+     12.6 search file name  
+        scoop install fd  
+     12.7 search file content  
+        scoop install ripgrep  
+     12.8 fuzzy search file  
+        scoop install fzf  
+     - 配置文件路径  
+     'C:\Users\ThinkPad\AppData\Roaming\yazi\config'  
+     - 自动代码预览，无需安装bat;wezterm自动图片预览  
+     - 若要通过mpv播放视频,需要将mpv的安装路径增加到环境变量中的'用户变量'  
+       如D:\Program Files\mpv-x86_64-20220424-git-9d133eb加入到'用户变量'  
+
+# usage
+## z
+  - cd xx/xx/xx/a1
+  - cd yy/yy/yy/b1
+  - z a1
+  - z b1
+  - z a
+  - z b
+
+## scoop
+
+   - 搜索软件  
+     scoop search [packageName]
+
+   - 安装软件  
+     scoop install [packageName]
+
+   - 指定安装版本  
+     scoop install [packageName]@version   
+
+   - 移除软件  
+     scoop uninstall [packageName]
+
+   - Show status and check for new app versions  
+     scoop status  
+
+   - To update all installed packages:  
+     scoop update *
+
+   - To list installed packages:  
+     scoop list
+
+   - To display information about a package:  
+     scoop info [packageName]
+
+   - To list all known buckets (a bucket is an application repository):  
+     scoop bucket known
+
+   - 显示关联的bucket
+     scoop bucket list
+
+   - To add a bucket by its alias or a Git repository URL:  
+     scoop bucket add [bucket]
+
+   - 删除仓库  
+     scoop bucket rm main  
+
+   --
+
+   - To install scoop (requires powershell >= 3):  
+   iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+
+   - To uninstall scoop:  
+   scoop uninstall scoop
+
+   - To update scoop:  
+   scoop update
+
+   - To install several utils at once:  
+   scoop install 7zip curl sudo git openssh coreutils grep sed less
+
+   - To install several programming languages at once:  
+   scoop install python ruby go nodejs
+
+   - To install php (see https://github.com/lukesampson/scoop/wiki/Custom-PHP-configuration for persisting php.ini settings through updates):  
+   scoop install php
+
+   - To install an app (Git) globally:  
+   sudo scoop install git --global
+
+   - To update a globally installed app (Git):  
+   sudo scoop update git --global
+
+   - Cleanup apps by removing old versions
+   scoop cleanup *
+
+   - Show or clear the download cache
+   scoop cache rm *
+
+## winfetch
+  - wf (winfetch)
+
+## yazi
+
+- a XX    创建XX文件  
+- a XX/   创建XX文件夹  
+
+---
+- y    copy  
+- d    cut  
+- x    delete to 回收站  
+- X    永久删除  
+- r    rename  
+       可以通过多选后，r重命名，批量化修改名字  
+
+---
+- g XX         移动到已存dir  
+- g <space>    目录跳转  
+
+---
+- cd   copy parent directory  
+- <A-PageUp>    预览上移动  
+- <A-PageDown>  预览下移动  
+
+---
+- S  rg搜索文件内容  
+- s  fd搜索文件名  
+- Z  fzf模糊搜索  
+
+---
+- o        打开  
+- Enter    打开  
+- O        选择打开方式  
+- C-Enter  选择打开方式  
+  Reveal   用'我的电脑'打开  
+
+将scoop中的main源从gitee切换至github  
+"scoop bucket list"  
+"scoop bucket rm main"  
+"scoop bucket add main https://github.com/ScoopInstaller/Main"  
+
+
+
+
+
