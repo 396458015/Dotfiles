bf2a97c378437a58ebc406f780ffc846d23e5cc5
396458015 <396458015@qq.com>
2025-02-09 23:25:49 +0800

    add yazi

---
+++ yazi/config/init.lua
@@ -0,0 +1,28 @@
+-- plugin
+-- starship.yazi
+require("starship"):setup()
+
+-- searchjump.yazi
+require("searchjump"):setup {
+	unmatch_fg = "#9ca0b0",
+    match_str_fg = "#000000",
+    match_str_bg = "#84c443",
+    first_match_str_fg = "#000000",
+    first_match_str_bg = "#84c443",
+    lable_fg = "#eeeeee",
+    lable_bg = "#b34607",
+    only_current = false, -- only search the current window
+    show_search_in_statusbar = true,
+    auto_exit_when_unmatch = true,
+    enable_capital_lable = false,
+    search_patterns = {}  -- demo:{"%.e%d+","s%d+e%d+"}
+}
+
+-- git.yazi
+THEME.git = THEME.git or {}
+THEME.git.modified = ui.Style():fg("#26deff")
+THEME.git.added = ui.Style():fg("#a6d189")
+require("git"):setup()
+
+-- mine-preview.yazi
+require("mime-preview"):setup()
