a9db9f1ad55ef009224c32a1777c707f1e1062d7
396458015 <396458015@qq.com>
2026-08-16 02:25:35 +0800

    update powershell config

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -110,7 +110,7 @@ function list_tree{ tree /F }
 Set-Alias -Name lt -Value list_tree
 
 # Computer info
-Set-Alias f winfetch
+Set-Alias ff winfetch
 
 # Basic commands
 function touch($file) { "" | Out-File $file -Encoding ASCII }
